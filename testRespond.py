4ec80b8900b27ed12a9c0ea14287a647aa24622b
zvicklan <zvicklan@andrew.cmu.edu>
2022-03-30 16:29:48 -0400

    Disable before exiting

--- testRespond.py
+++ testRespond.py
@@ -17,6 +17,8 @@ txdevice = None
 
 # pylint: disable=unused-argument
 def exithandler(signal, frame):
+    rxdevice.enable_rx()
+    txdevice.enable_tx()
     rxdevice.cleanup()
     txdevice.cleanup()
     sys.exit(0)
