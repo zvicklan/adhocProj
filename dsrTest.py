f22e50d22bc62062dbad26e09ea29fa4c1f59b89
zvicklan <zvicklan@andrew.cmu.edu>
2022-04-11 15:08:19 -0400

    Fixing printing

--- dsrTest.py
+++ dsrTest.py
@@ -114,7 +114,7 @@ while not(testDone):
                     #If this is a shorter path than I previously had, or it's a new msg
                     if hops2Node[srcID-1] > hopCount or msgID != lastMsgID:
                         path2Node, hops2Node = updateCache(path2Node, hops2Node, myID, origID, destID, pathFromOrig)
-                        logging.info("Got Route Disc. Updated routing cache to " + str(path2Node[destID-1]))
+                        logging.info("Got Route Disc. Updated routing cache to " + str(path2Node[origID-1]))
                         
                         msg = makeMsgRouteReply(origID, msgID, myID, destID, pathFromOrig)
                         logging.info("Got Route Disc. Sending Reply msg " + hex(msg))
