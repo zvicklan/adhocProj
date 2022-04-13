35a929629309deb292e1e45fa2ea21b738cf60b8
zvicklan <zvicklan@andrew.cmu.edu>
2022-04-13 15:39:10 -0400

    Need right function name

--- dsrTest.py
+++ dsrTest.py
@@ -85,7 +85,7 @@ while not(testDone):
                      ", msgType " + str(msgType) + "]")
         
         #We will do processing if this is a real message
-        if msgType and isAck(rxMsg): #It's a real msg and an ACK (I'm not in the right state, so skip)
+        if msgType and isAckMsg(rxMsg): #It's a real msg and an ACK (I'm not in the right state, so skip)
             continue
         if msgType == 1: #Route Discovery
             (origID, msgID, srcID, destID, hopCount, pathFromOrig) = readMsgRouteDisc(rxMsg)
